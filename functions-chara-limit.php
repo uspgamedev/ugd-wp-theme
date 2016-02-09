084db2d6cdee439ec6a608365f7fb409b7ab7675
Willian Akira <Akira@whitechocolate.home>
2016-02-09 03:19:31 -0200

    fixed bug in post title filter

--- functions-chara-limit.php
+++ functions-chara-limit.php
@@ -12,7 +12,13 @@ function UGD_limit_post_title($title, $post_id = null) {
 	$LIMIT = 32;
 	
 	if ( !is_single() && strlen($title) > $LIMIT ) {
-		$title = substr($title, 0, $LIMIT) . "...";
+		if ( preg_match('/[^\x20-\x7f]/', $title[$LIMIT-1] ) ) {
+			// This checks to see if the last character is half a utf-8 character.
+			// Since accentuated letters occupy two bytes, this is necessary.
+			// Otherwise garbage is displayed.
+			$LIMIT += 1;
+		}
+		$title =  substr($title, 0, $LIMIT) . "...";
 	}
 
 	return $title;
