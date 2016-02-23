7db42376ea59c2dc96e3c5a597674b85b38afa67
orenjiakira <orenjiakira@gmail.com>
2016-02-23 13:34:27 -0300

    yo ho ho

--- README.md
+++ README.md
@@ -2,16 +2,5 @@
 Tema de WordPress para o layout do blog.
 Deve ser clonado em wp-content/themes/ na pasta do site no servidor.
 
-# Olá
-Coisas a fazer:
-<div>
-a.Criar branches de production e development. E depois branchear isso aí further.
-</div><div>
-b.Colocar alguma coisa no repo.
-</div><div>
-c.Coisas.
-</div><div>
-d.????????
-</div><div>
-e.Yay
-</div>
+## Olha só, tá funfando
+Se quiser alguma feature, só dar um issue.
--- section-cover.php
+++ section-cover.php
@@ -11,6 +11,7 @@
 		<div class="row text-center smaller">
 			<div class="col-xs-12">
 				<ul class="list-unstyled">
+					<li><span style="unicode-bidi: bidi-override; direction: rtl;">gro.vedemagpsu@otatnoc</span></li>
 					<li><?php echo sprintf( __( '%1$s %2$s %3$s. All Rights Reserved.', 'UGD' ), '&copy;', date( 'Y' ), esc_html( get_bloginfo( 'name' ) ) ); ?></li>
 					<li><?php echo sprintf( __( ' Theme By: %1$s.', 'UGD' ), '<a title="W. Akira" class="greenlue-text" href="http://github.com/orenjiakira">W. Akira</a>' ); ?></li>
 				</ul>
